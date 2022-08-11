67f6fb2030cd1239e16753d0038977f632311045
MariusIonutSipanu <sipanumarius3@gmail.com>
2022-08-11 11:41:07 +0300

    Second commit.

--- src/main/java/com/example/auction/AuctionApplication.java
+++ src/main/java/com/example/auction/AuctionApplication.java
@@ -8,6 +8,7 @@ public class AuctionApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AuctionApplication.class, args);
+        System.out.println("hello");
     }
 
 }
